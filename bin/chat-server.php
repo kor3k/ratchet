4399f9e4f541e6ea0bcc5a821fbcb4f9efd89caf
kor3k <martin.koranda@gmail.com>
2014-02-04 21:00:01 +0100

    telnet -> wsserver

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,12 +1,23 @@
 <?php
 
 use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
 use Test\Chat;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+//$server = IoServer::factory(
+//    new Chat(),
+//    8080
+//);
+
 $server = IoServer::factory(
-    new Chat(),
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
     8080
 );
 
