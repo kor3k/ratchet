91e4cd703e703e4de101f2a3c2ed985f10f68188
kor3k <martin.koranda@gmail.com>
2014-03-05 18:58:51 +0100

    stopping the loop in client

--- src/Test/Client.php
+++ src/Test/Client.php
@@ -3,9 +3,13 @@
 namespace Test;
 
 use Psr\Log\LoggerInterface;
+use React\EventLoop\LoopInterface;
 
 class Client implements \WebSocketClient\WebSocketClientInterface
 {
+    /**
+     * @var \WebSocketClient
+     */
     private $client;
 
     /**
@@ -13,14 +17,21 @@ class Client implements \WebSocketClient\WebSocketClientInterface
      */
     private $logger;
 
-    public function __construct( LoggerInterface $logger )
+    /**
+     * @var \React\EventLoop\LoopInterface
+     */
+    private $loop;
+
+    public function __construct( LoggerInterface $logger , LoopInterface $loop )
     {
         $this->logger   =   $logger;
+        $this->loop     =   $loop;
     }
 
     public function onWelcome(array $data)
     {
-        $this->logger->info( 'welcome' , $data );
+        $this->logger->info( 'connected' , $data );
+
         $this->subscribe( 'kittensCategory' );
         $this->publish( 'kittensCategory' , 'jupijou' );
     }
@@ -28,6 +39,7 @@ class Client implements \WebSocketClient\WebSocketClientInterface
     public function onEvent($topic, $message)
     {
         $this->logger->info( 'event' , [ 'topic' => $topic , 'message' => $message ] );
+        $this->loop->stop();
     }
 
     public function subscribe($topic)
@@ -57,6 +69,15 @@ class Client implements \WebSocketClient\WebSocketClientInterface
         $this->logger->info( 'publish' , [ 'topic' => $topic , 'message' => $message ] );
     }
 
+    public function disconnect()
+    {
+        $this->logger->info( 'disconnected' );
+        $this->client->disconnect();
+    }
+
+    /**
+     * @param \WebSocketClient $client
+     */
     public function setClient(\WebSocketClient $client)
     {
         $this->client = $client;
--- web/client.php
+++ web/client.php
@@ -4,9 +4,12 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 $logger = new \Monolog\Logger( 'wsclient' );
 $logger->pushHandler( new \Monolog\Handler\StreamHandler( dirname(__DIR__).'/logs/client.log' ) );
+$logger->pushHandler( new \Monolog\Handler\StreamHandler( 'php://output' ) );
+
 
 $loop = React\EventLoop\Factory::create();
-$client = new WebSocketClient( new \Test\Client( $logger ) , $loop );
+$client = new WebSocketClient( new \Test\Client( $logger , $loop ) , $loop );
 
 $loop->run();
 
+$client->getClient()->disconnect();
